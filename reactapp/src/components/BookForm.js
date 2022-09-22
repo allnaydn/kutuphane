afb8b4ada398e6d05a16b9505d812708431a06ac
allnaydn <112631495+allnaydn@users.noreply.github.com>
2022-09-22 14:43:50 +0300

    Update BookForm.js
    commit test

--- reactapp/src/components/BookForm.js
+++ reactapp/src/components/BookForm.js
@@ -26,7 +26,7 @@ export default function BookForm() {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        <span>Yeni Kitap Oluştur</span>
+        <span>Yeni Kitap Ekle</span>
         <input 
           required
           type="text"
